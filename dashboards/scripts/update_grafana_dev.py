36599879d462db5cd2e98e623f8197b9a7653c3f
Myra <myra.haubrich@gmail.com>
2018-09-25 20:51:47 -0700

    Update update_grafana_dev.py

--- dashboards/scripts/update_grafana_dev.py
+++ dashboards/scripts/update_grafana_dev.py
@@ -13,14 +13,14 @@ dsurl = 'http://0.0.0.0:3000/api/datasources'
 
 dburl = 'http://0.0.0.0:3000/api/dashboards/db'
 
-db_list = glob.glob("/Users/haubrich/Downloads/grafana-db/*.json")
+db_list = glob.glob("/var/jenkins_home/workspace/grafana-test/dashboards/dev/*.json")
 print(db_list)
 for db in db_list:
     contentdb = open(db, 'rb').read()
     response = requests.post(dburl, data=contentdb, headers=headers)
     print(response.text)
 
-ds_list = glob.glob("/Users/haubrich/Downloads/grafana-ds/*.json")
+ds_list = glob.glob("/var/jenkins_home/workspace/grafana-test/datasources/dev/*.json")
 print(ds_list)
 for ds in ds_list:
     contentds = open(ds, 'rb').read()
